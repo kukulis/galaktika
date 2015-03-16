efd117c3be4b475d23cc139d498060bc49fce91b
Giedrius Tumelis <giedrius.tumelis@atea.lt>
2015-03-16 17:11:53 +0200

    Error like try to make relation between objects.

--- src/main/java/lt/gt/sgalaktika/battle/BattleReportRound.java
+++ src/main/java/lt/gt/sgalaktika/battle/BattleReportRound.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BattleReportRound {
+	private long id;
 	private int roundNumber;
 	private List < BattleReportShot> shots = new ArrayList<BattleReportShot>();
 	
+	public BattleReportRound() {
+	}
 	public int getRoundNumber() {
 		return roundNumber;
 	}
@@ -15,5 +18,14 @@ public class BattleReportRound {
 	}
 	public List<BattleReportShot> getShots() {
 		return shots;
+	}
+	public void setShots(List<BattleReportShot> shots) {
+		this.shots = shots;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
 	}
 }
