ef40333a199b2949096acd225bac68467c02ded6
Giedrius Tumelis <giedrius.tumelis@atea.lt>
2015-04-24 17:10:31 +0300

    Planavimas dėl laivynų.

--- src/main/java/lt/gt/sgalaktika/space/Race.java
+++ src/main/java/lt/gt/sgalaktika/space/Race.java
@@ -1,10 +1,9 @@
 package lt.gt.sgalaktika.space;
 
-import lt.gt.sgalaktika.construction.Technologies;
 
 public class Race {
 	private String name;
-	private Technologies technologies;
+	
 	
 	public String getName() {
 		return name;
@@ -12,10 +11,5 @@ public class Race {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Technologies getTechnologies() {
-		return technologies;
-	}
-	public void setTechnologies(Technologies technologies) {
-		this.technologies = technologies;
-	}
+	
 }
