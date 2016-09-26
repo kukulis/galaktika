1702d74e2acd7bb40ee5077817d587aea68c58c2
Giedrius <giedriustum@gmail.com>
2016-09-26 23:02:33 +0300

    Tasks for future (Planet Surface storing and loading ).

--- galaktika-parent/galaktika-model/src/test/java/lt/gt/galaktika/model/test/memory/service/TestMemoryPlanetDataService.java
+++ galaktika-parent/galaktika-model/src/test/java/lt/gt/galaktika/model/test/memory/service/TestMemoryPlanetDataService.java
@@ -1,7 +1,6 @@
 package lt.gt.galaktika.model.test.memory.service;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
