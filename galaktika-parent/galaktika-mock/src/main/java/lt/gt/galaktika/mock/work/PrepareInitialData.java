0131b982071bf7ec5202359c1f3877f8d153323a
Giedrius Tumelis <giedriustum@gmail.com>
2017-03-03 01:03:05 +0200

    Initialization code structure.

---
+++ galaktika-parent/galaktika-mock/src/main/java/lt/gt/galaktika/mock/work/PrepareInitialData.java
@@ -0,0 +1,17 @@
+package lt.gt.galaktika.mock.work;
+
+import org.junit.Test;
+
+/**
+ * This class will be used to create initial data in the database.
+ * @author Lenovo
+ *
+ */
+public class PrepareInitialData {
+	
+	@Test
+	public void createInitialUsers() {
+		System.out.println( "createInitialUsers called" );
+		// TODO
+	}
+}
